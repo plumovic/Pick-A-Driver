467e621187ee472429e1fcc05b6ecfd70a1e91e8
Petar Lumovic <plumovic@petars-macbook-air.local>
2018-02-06 12:03:09 -0600

    Initial Commit

---
+++ Pick A DriverTests/Pick_A_DriverTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pick_A_DriverTests.swift
+//  Pick A DriverTests
+//
+//  Created by Petar Lumovic on 2/6/18.
+//  Copyright © 2018 Petar Lumovic. All rights reserved.
+//
+
+import XCTest
+@testable import Pick_A_Driver
+
+class Pick_A_DriverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
