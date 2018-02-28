a56404456422aae052e425c3e38c0e7b5777653e
Andrew Kinsey <akinsey@andrews-macbook-air-123.local>
2018-02-28 12:29:01 -0600

    Name deletion

--- Pick A Driver/DisplayViewController.swift
+++ Pick A Driver/DisplayViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class DisplayViewController: UIViewController
 {
     @IBOutlet var nameLabels: [UILabel]!
+    @IBOutlet weak var startButton: UIButton!
     
     var period = String()
     var names = [String]()
+    var timer = Timer()
+    var cycle = Double()
     
     let defaults = UserDefaults.standard
 
@@ -40,6 +43,56 @@ class DisplayViewController: UIViewController
         {
             nameLabels[i].text = names[i]
         }
+        cycle = 2.0
+    }
+    
+    func resetTimer()
+    {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: cycle, target: self, selector: #selector(updateDisplay), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateDisplay()
+    {
+        resetTimer()
+        var nameLabelIndices = [Int]()
+        for i in 0..<nameLabels.count
+        {
+            if nameLabels[i].text != ""
+            {
+                nameLabelIndices.append(i)
+            }
+        }
+        let randomPick = Int(arc4random_uniform(UInt32(nameLabelIndices.count)))
+        nameLabels[nameLabelIndices[randomPick]].text = ""
+        if nameLabelIndices.count <= 2
+        {
+            timer.invalidate()
+            startButton.setTitle("Reset", for: .normal)
+            startButton.backgroundColor = UIColor.yellow
+        }
+    }
+    
+    @IBAction func onStartButtonTapped(_ sender: UIButton)
+    {
+        let title = sender.titleLabel!.text!
+        switch title
+        {
+        case "Start" :
+            resetTimer()
+            sender.setTitle("Stop", for: .normal)
+            sender.backgroundColor = UIColor.red
+        case "Stop" :
+            timer.invalidate()
+            sender.setTitle("Reset", for: .normal)
+            sender.backgroundColor = UIColor.yellow
+        default:
+            updateDisplay()
+            sender.setTitle("Start", for: .normal)
+            sender.backgroundColor = UIColor.green
+            resetNames()
+        }
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
